334e93ffd35e13ad6c990e84062a3da72cebc1f9
Liew Zhen Jie <32891669+chico000@users.noreply.github.com>
2020-05-23 20:15:43 +0800

    Updates

---
+++ precache-manifest.1e9104a68110b618c751b079cd9240fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c451c11992b0bc2bbf762384570d744",
+    "url": "/robofirends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofirends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ac1e8f1183eadb80ebd3",
+    "url": "/robofirends/static/css/main.f37703e1.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofirends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofirends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac1e8f1183eadb80ebd3",
+    "url": "/robofirends/static/js/main.cfd72e9c.chunk.js"
+  },
+  {
+    "revision": "909c763dfa24045f4408",
+    "url": "/robofirends/static/js/runtime-main.b04c6f16.js"
+  }
+]);
